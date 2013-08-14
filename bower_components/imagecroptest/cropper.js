b3c8738779dd97fc11350ec0969b6b9564cf2d5a
Mateusz Chrobok <mateusz.chrobok@gmail.com>
2013-08-14 14:21:04 +0200

    Added bower dependecies

---
+++ bower_components/imagecroptest/cropper.js
@@ -0,0 +1,39 @@
+var imageCropper;
+
+$(document).ready(function () {
+  imageCropper = new ImageCropper();
+});
+
+function ImageCropper() {
+
+  var cropper;
+  this.imageLoadedCallback;
+
+  this.imageLoaded = function () {
+    this.imageLoadedCallback();
+  };
+
+  this.aspectRatioChanged = function (combobox) {
+    $('#image').Jcrop({
+      aspectRatio: combobox.options[combobox.selectedIndex].value
+    });
+  };
+
+  this.crop = function () {
+    var coords = cropper.tellSelect();
+    $('#height').text('height: ' + coords.h + ', width: ' + coords.w);
+  };
+
+  this.reload = function (src) {
+    cropper.setImage(src);
+  };
+
+  $('#image').Jcrop({ aspectRatio: 4 / 3 }, function () {
+    cropper = this;
+  });
+};
+
+loadImage = function (src, callback) {
+  this.imageLoadedCallback = callback;
+  imageCropper.reload(src);
+};
