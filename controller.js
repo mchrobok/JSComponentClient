2627c157f338fcb48b6d607aa8b429dbc9b7edec
Mateusz Chrobok <mateusz.chrobok@gmail.com>
2013-08-14 10:45:33 +0200

    Added styles, loading default image at the beggining

--- controller.js
+++ controller.js
@@ -1,3 +1,20 @@
 function Controller ($scope) {
-  $("#includedContent").load('bower_components/imagecroptest/cropper.html');
+  $scope.load = function () {
+    var F = document.getElementById("widget");
+    if ($('#url').val() !== '') {
+      F.contentWindow.loadImage($('#url').val(), imageLoadedCallback);
+    }
+  };
+
+  function imageLoadedCallback () {
+    var F = document.getElementById("widget");
+    F.height = F.contentDocument.documentElement.scrollHeight;
+    F.width = F.contentDocument.documentElement.scrollWidth;
+  }
+
+  $('#widget').load(function () {
+    $('#url').val('http://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Turkish_Van_Cat.jpg/700px-Turkish_Van_Cat.jpg');
+    $scope.load();
+  });
+  $('#widget').attr('src','bower_components/imagecroptest/cropper.html');
 }
